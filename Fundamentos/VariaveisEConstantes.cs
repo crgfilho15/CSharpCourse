1d9f12639950d9c4f13681bacf28c1bc57a85eb7
Carlos <0001038504@senaimgaluno.com.br>
2024-01-27 23:47:13 -0300

    first commit

---
+++ Fundamentos/VariaveisEConstantes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    class VariaveisEConstantes
+    {
+        public static void Executar()
+        {
+
+        }
+    }
+}
